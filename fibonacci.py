4d1100128c68f152d43630780bbb10d8e1f9e77f
Felix Brucker <bruckerl@oregonstate.edu>
2021-02-25 23:12:56 -0800

    Factorial update

--- fibonacci.py
+++ fibonacci.py
@@ -6,3 +6,9 @@ def fib(x):
         new_int = f[len(f) - 1] + f[len(f) - 2]
         f.append( new_int )
     return f
+
+def factorial(x):
+    z = 1
+    for y in range(1,x+1):
+        z *= y
+    return z
--- test_fibonacci.py
+++ test_fibonacci.py
@@ -5,6 +5,9 @@ class TestFibonacciMethods(unittest.TestCase):
     def test_fib(self):
         result = fibonacci.fib(6)
         self.assertEqual(result, [0,1,1,2,3,5])
+    def test_factorial(self):
+        result = fibonacci.factorial(6)
+        self.assertEqual(result, 720)
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
